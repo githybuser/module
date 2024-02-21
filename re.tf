34b168b69c53eb8b54b789b77e560ae9f821c28d
githybuser <157712966+githybuser@users.noreply.github.com>
2024-02-21 15:29:32 +0000

    delete

--- re.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_launch_configuration" "temp-home" {
-    name = "${var.project}-temp-home"
-    iamge_id = var.iamge_id
-    instance_type = var.instance_type
-    key_name = var.key_name
-    security_groups = var.security_groups
-    
-}
