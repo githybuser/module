89c81d4b6f43f4e8d4bd3c03047c0e65d14b3850
githybuser <157712966+githybuser@users.noreply.github.com>
2024-02-21 18:18:40 +0000

    alldone

--- resourse.tf
+++ resourse.tf
@@ -4,15 +4,10 @@ provider "aws"{
 resource "aws_eks_cluster" "practice-cluster" {
     name = "${var.project}-cluster"
     role_arn = var.role_arn 
-    vpc_config {
-    endpoint_private_access = true
-    endpoint_public_access  = false
-    }
     access_config {
         authentication_mode = "API_AND_CONFIG_MAP"  
-        bootstrap_cluster_creator_permission = true  
+        bootstrap_cluster_creator_admin_permission = true  
     }
-    
     vpc_config {
         security_group_ids = var.security_group_ids
         subnet_ids = var.subnet_ids 
