332eef87892ff97af6db928695f2a11869efa88c
githybuser <157712966+githybuser@users.noreply.github.com>
2024-02-24 18:15:19 +0530

    Update main.tf

--- data_sourse/main.tf
+++ data_sourse/main.tf
@@ -32,5 +32,7 @@ resource "aws_security_group_rule" "all-tcp" {
 
 }
 
-output = var.my_instance_private_ip
+output "my_instance_private_ip" {
+    value = aws_instance.my_instance.private_ip
+}
     
